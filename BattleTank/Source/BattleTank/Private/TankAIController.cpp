df450ca1c8d4b87de86b3e2e03b4be72683396f9
Rijkman <rijkmanpilaar@gmail.com>
2019-09-28 18:36:51 +0200

    Loggin Tanks

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	auto PlayerController = GetPlayerTank();
+	if (PlayerController)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI Found: %s"), *(PlayerController->GetName()))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Cannot find PlayerController"));
+	}
+}
+
+ATank* ATankAIController::GetControlledTank()
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank()
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (PlayerPawn)
+	{
+		return Cast<ATank>(PlayerPawn);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Cannot find PlayerController"));
+		return 0;
+	}
+}
+
+
